d21dd0e29837849c816994f80b7d6c6aef9198b3
sashi <satoshi3104.nishi@gmail.com>
2025-06-07 15:56:48 +0900

    Sleep 実装

--- Tsuyu/TY/System.cpp
+++ Tsuyu/TY/System.cpp
@@ -11,43 +11,15 @@ namespace
 {
     struct SystemState
     {
-        std::optional<double> targetFrameRate{};
     } s_system{};
-
-    void sleepForTargetFrameRate()
-    {
-        // TODO: Timer 修正
-        if (const auto targetFrameRate = s_system.targetFrameRate)
-        {
-            const double targetDeltaTime = 1.0 / *targetFrameRate;
-            const double actualDeltaTime = EngineTimer.GetDeltaTime();
-            if (actualDeltaTime < targetDeltaTime)
-            {
-                const double remaining = targetDeltaTime - actualDeltaTime;
-                ::Sleep(static_cast<DWORD>(remaining * 1000.0));
-            }
-        }
-    }
 }
 
 namespace TY
 {
     using namespace detail;
 
-    void System::SetTargetFrameRate(std::optional<double> frameRate)
-    {
-        s_system.targetFrameRate = frameRate;
-    }
-
-    std::optional<double> System::TargetFrameRate()
-    {
-        return s_system.targetFrameRate;
-    }
-
     bool System::Update()
     {
-        sleepForTargetFrameRate();
-
         if (EngineCore.IsInFrame()) EngineCore.EndFrame();
 
         MSG msg;
@@ -71,6 +43,14 @@ namespace TY
     uint64_t System::FrameCount()
     {
         return EngineTimer.GetFrameCount();
+    }
+
+    void System::Sleep(uint64_t ms)
+    {
+        if (ms > 0)
+        {
+            ::Sleep(static_cast<DWORD>(ms));
+        }
     }
 
     void System::ModalError(const std::wstring& message)
