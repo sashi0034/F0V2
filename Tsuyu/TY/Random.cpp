f2ce25f99cb4fea7e0dbb7c016408e184c914be9
sashi <satoshi3104.nishi@gmail.com>
2025-06-17 21:56:11 +0900

    Random 実装

---
+++ Tsuyu/TY/Random.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "Random.h"
+
+namespace
+{
+    thread_local std::mt19937 s_engine{std::random_device{}()};
+}
+
+namespace TY
+{
+    int Random::Int(int min, int max)
+    {
+        std::uniform_int_distribution<int> dist(min, max);
+        return dist(s_engine);
+    }
+
+    float Random::Float(float min, float max)
+    {
+        std::uniform_real_distribution<float> dist(min, max);
+        return dist(s_engine);
+    }
+
+    bool Random::Bool()
+    {
+        std::uniform_int_distribution<int> dist(0, 1);
+        return dist(s_engine) == 1;
+    }
+}
---
+++ Tsuyu/TY/Random.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+namespace TY
+{
+    namespace Random
+    {
+        int Int(int min, int max);
+
+        float Float(float min, float max);
+
+        bool Bool();
+    }
+}
