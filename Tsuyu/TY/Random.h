1b15ef4314122022830a62f19a91d5823ad7a90c
sashi <satoshi3104.nishi@gmail.com>
2025-06-18 17:54:33 +0900

    Shuffle

--- Tsuyu/TY/Random.h
+++ Tsuyu/TY/Random.h
@@ -9,5 +9,15 @@ namespace TY
         float Float(float min, float max);
 
         bool Bool();
+
+        template <typename Container>
+        void Shuffle(Container& array)
+        {
+            for (size_t i = array.size() - 1; i > 0; --i)
+            {
+                size_t j = Int(0, static_cast<int>(i));
+                std::swap(array[i], array[j]);
+            }
+        }
     }
 }
