eb22b018d76d5a15b224d4bc0e971cf21ba9ec23
sashi <satoshi3104.nishi@gmail.com>
2025-06-19 16:11:23 +0900

    detail 付与

--- Tsuyu/TY/GpgpuBuffer1D.h
+++ Tsuyu/TY/GpgpuBuffer1D.h
@@ -4,167 +4,170 @@
 
 namespace TY
 {
-    struct IGpgpuBuffer
+    namespace detail
     {
-        virtual ~IGpgpuBuffer() = default;
-
-        virtual void* getDataPointer() = 0;
-
-        virtual int getElementCount() const = 0;
-
-        virtual int getElementStride() const = 0;
-
-        virtual Point3D getSize3D() const = 0;
-    };
-
-    struct IWritableGpgpu : IGpgpuBuffer
-    {
-    };
-
-    struct IReadonlyGpgpu : IGpgpuBuffer
-    {
-    };
-
-    template <typename DataType, typename InterfaceType>
-    class GpgpuBuffer1D
-    {
-    public:
-        static_assert(std::is_base_of<IGpgpuBuffer, InterfaceType>::value);
-
-        static constexpr int ElementStride = sizeof(DataType);
-
-        GpgpuBuffer1D()
+        struct IGpgpuBuffer
         {
-        }
+            virtual ~IGpgpuBuffer() = default;
 
-        GpgpuBuffer1D(int elementCount) : p_impl(std::make_shared<Impl>(elementCount))
+            virtual void* getDataPointer() = 0;
+
+            virtual int getElementCount() const = 0;
+
+            virtual int getElementStride() const = 0;
+
+            virtual Point3D getSize3D() const = 0;
+        };
+
+        struct IWritableGpgpu : IGpgpuBuffer
         {
-        }
+        };
 
-        operator std::shared_ptr<InterfaceType>()
+        struct IReadonlyGpgpu : IGpgpuBuffer
         {
-            return p_impl;
-        }
+        };
 
-        const Array<DataType>& data() const
+        template <typename DataType, typename InterfaceType>
+        class GpgpuBuffer1D
         {
-            return p_impl->m_data;
-        }
+        public:
+            static_assert(std::is_base_of<IGpgpuBuffer, InterfaceType>::value);
 
-        Array<DataType>& data()
-        {
-            return p_impl->m_data;
-        }
+            static constexpr int ElementStride = sizeof(DataType);
 
-    private:
-        struct Impl : InterfaceType
-        {
-            int m_elementCount{};
-            Array<DataType> m_data{};
-
-            Impl() = default;
-
-            Impl(int count)
-                : m_elementCount(count),
-                  m_data(count)
-
+            GpgpuBuffer1D()
             {
             }
 
-            void* getDataPointer() override { return m_data.data(); }
-
-            int getElementCount() const override { return m_elementCount; }
-
-            int getElementStride() const override { return ElementStride; }
-
-            Point3D getSize3D() const override { return Point3D(m_elementCount, 1, 1); }
-        };
-
-        std::shared_ptr<Impl> p_impl{};
-
-        GpgpuBuffer1D(const std::shared_ptr<Impl>& impl) : p_impl(impl)
-        {
-        }
-    };
-
-    template <typename DataType, typename InterfaceType>
-    class GpgpuBuffer2D
-    {
-    public:
-        static_assert(std::is_base_of<IGpgpuBuffer, InterfaceType>::value);
-
-        static constexpr int ElementStride = sizeof(DataType);
-
-        GpgpuBuffer2D()
-        {
-        }
-
-        GpgpuBuffer2D(int x, int y) : p_impl(std::make_shared<Impl>(x, y))
-        {
-        }
-
-        operator std::shared_ptr<InterfaceType>()
-        {
-            return p_impl;
-        }
-
-        const Array<DataType>& data() const
-        {
-            return p_impl->m_data;
-        }
-
-        Array<DataType>& data()
-        {
-            return p_impl->m_data;
-        }
-
-        DataType& at(int x, int y)
-        {
-            return p_impl->m_data[p_impl->m_sizeX * y + x];
-        }
-
-        const DataType& at(int x, int y) const
-        {
-            return p_impl->m_data[p_impl->m_sizeX * y + x];
-        }
-
-    private:
-        struct Impl : InterfaceType
-        {
-            int m_sizeX{};
-            int m_sizeY{};
-            int m_elementCount{};
-            Array<DataType> m_data{};
-
-            Impl() = default;
-
-            Impl(int x, int y)
-                : m_sizeX(x),
-                  m_sizeY(y),
-                  m_elementCount(x * y),
-                  m_data(m_elementCount)
+            GpgpuBuffer1D(int elementCount) : p_impl(std::make_shared<Impl>(elementCount))
             {
             }
 
-            void* getDataPointer() override { return m_data.data(); }
+            operator std::shared_ptr<InterfaceType>()
+            {
+                return p_impl;
+            }
 
-            int getElementCount() const override { return m_elementCount; }
+            const Array<DataType>& data() const
+            {
+                return p_impl->m_data;
+            }
 
-            int getElementStride() const override { return ElementStride; }
+            Array<DataType>& data()
+            {
+                return p_impl->m_data;
+            }
 
-            Point3D getSize3D() const override { return Point3D(m_sizeX, m_sizeY, 1); }
+        private:
+            struct Impl : InterfaceType
+            {
+                int m_elementCount{};
+                Array<DataType> m_data{};
+
+                Impl() = default;
+
+                Impl(int count)
+                    : m_elementCount(count),
+                      m_data(count)
+
+                {
+                }
+
+                void* getDataPointer() override { return m_data.data(); }
+
+                int getElementCount() const override { return m_elementCount; }
+
+                int getElementStride() const override { return ElementStride; }
+
+                Point3D getSize3D() const override { return Point3D(m_elementCount, 1, 1); }
+            };
+
+            std::shared_ptr<Impl> p_impl{};
+
+            GpgpuBuffer1D(const std::shared_ptr<Impl>& impl) : p_impl(impl)
+            {
+            }
         };
 
-        std::shared_ptr<Impl> p_impl{};
-
-        GpgpuBuffer2D(const std::shared_ptr<Impl>& impl) : p_impl(impl)
+        template <typename DataType, typename InterfaceType>
+        class GpgpuBuffer2D
         {
-        }
-    };
+        public:
+            static_assert(std::is_base_of<IGpgpuBuffer, InterfaceType>::value);
+
+            static constexpr int ElementStride = sizeof(DataType);
+
+            GpgpuBuffer2D()
+            {
+            }
+
+            GpgpuBuffer2D(int x, int y) : p_impl(std::make_shared<Impl>(x, y))
+            {
+            }
+
+            operator std::shared_ptr<InterfaceType>()
+            {
+                return p_impl;
+            }
+
+            const Array<DataType>& data() const
+            {
+                return p_impl->m_data;
+            }
+
+            Array<DataType>& data()
+            {
+                return p_impl->m_data;
+            }
+
+            DataType& at(int x, int y)
+            {
+                return p_impl->m_data[p_impl->m_sizeX * y + x];
+            }
+
+            const DataType& at(int x, int y) const
+            {
+                return p_impl->m_data[p_impl->m_sizeX * y + x];
+            }
+
+        private:
+            struct Impl : InterfaceType
+            {
+                int m_sizeX{};
+                int m_sizeY{};
+                int m_elementCount{};
+                Array<DataType> m_data{};
+
+                Impl() = default;
+
+                Impl(int x, int y)
+                    : m_sizeX(x),
+                      m_sizeY(y),
+                      m_elementCount(x * y),
+                      m_data(m_elementCount)
+                {
+                }
+
+                void* getDataPointer() override { return m_data.data(); }
+
+                int getElementCount() const override { return m_elementCount; }
+
+                int getElementStride() const override { return ElementStride; }
+
+                Point3D getSize3D() const override { return Point3D(m_sizeX, m_sizeY, 1); }
+            };
+
+            std::shared_ptr<Impl> p_impl{};
+
+            GpgpuBuffer2D(const std::shared_ptr<Impl>& impl) : p_impl(impl)
+            {
+            }
+        };
+    }
 
     template <typename DataType>
-    using WritableGpgpuBuffer1D = GpgpuBuffer1D<DataType, IWritableGpgpu>;
+    using WritableGpgpuBuffer1D = detail::GpgpuBuffer1D<DataType, detail::IWritableGpgpu>;
 
     template <typename DataType>
-    using ReadonlyGpgpuBuffer1D = GpgpuBuffer1D<DataType, IReadonlyGpgpu>;
+    using ReadonlyGpgpuBuffer1D = detail::GpgpuBuffer1D<DataType, detail::IReadonlyGpgpu>;
 }
