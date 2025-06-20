1e6a8e4bc4156eecd997ad6251509028d4dd07cd
sashi <satoshi3104.nishi@gmail.com>
2025-06-20 15:32:50 +0900

    ScopedDeferStack 追加

---
+++ Tsuyu/TY/ScopedDeferStack.cpp
@@ -0,0 +1,53 @@
+﻿#include "pch.h"
+#include "ScopedDeferStack.h"
+
+namespace TY
+{
+    ScopedDeferStack::ScopedDeferStack(ScopedDeferStack&& other) noexcept
+        : m_disposables(std::move(other.m_disposables)),
+          m_active(other.m_active)
+    {
+        other.m_active = false;
+    }
+
+    ScopedDeferStack& ScopedDeferStack::operator=(ScopedDeferStack&& other) noexcept
+    {
+        if (this != &other)
+        {
+            m_disposables = std::move(other.m_disposables);
+            m_active = other.m_active;
+            other.m_active = false;
+        }
+
+        return *this;
+    }
+
+    ScopedDeferStack::~ScopedDeferStack()
+    {
+        if (m_active)
+        {
+            dispose();
+        }
+    }
+
+    void ScopedDeferStack::dispose()
+    {
+        if (!m_active) return;
+
+        // 後入れ先出し順に dispose
+        for (auto it = m_disposables.rbegin(); it != m_disposables.rend(); ++it)
+        {
+            it->dispose();
+        }
+
+        m_disposables.clear();
+        m_active = false;
+    }
+
+    ScopedDeferStack ScopedDeferStack::push(ScopedDefer&& disposable)
+    {
+        m_disposables.emplace_back(std::move(disposable));
+        m_active = true;
+        return std::move(*this);
+    }
+}
---
+++ Tsuyu/TY/ScopedDeferStack.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include "ScopedDefer.h"
+
+namespace TY
+{
+    class ScopedDeferStack
+    {
+    public:
+        ScopedDeferStack() = default;
+
+        ScopedDeferStack(const ScopedDeferStack&) = delete;
+
+        ScopedDeferStack& operator=(const ScopedDeferStack&) = delete;
+
+        ScopedDeferStack(ScopedDeferStack&& other) noexcept;
+
+        ScopedDeferStack& operator=(ScopedDeferStack&& other) noexcept;
+
+        ~ScopedDeferStack();
+
+        void dispose();
+
+        ScopedDeferStack push(ScopedDefer&& disposable);
+
+        template <typename... Ts>
+        ScopedDeferStack push(ScopedDefer&& first, Ts&&... rest)
+        {
+            auto p = push(std::move(first));
+            ((p = p.push(std::move(rest))), ...);
+            return p;
+        }
+
+    private:
+        std::vector<ScopedDefer> m_disposables;
+        bool m_active;
+    };
+}
