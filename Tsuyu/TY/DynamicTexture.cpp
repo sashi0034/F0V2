b288702069ef6c5efc53e3224380d1acc6704e33
sashi <satoshi3104.nishi@gmail.com>
2025-06-24 14:28:59 +0900

    DynamicTexture 追加

--- Tsuyu/TY/DynamicTexture.cpp
+++ Tsuyu/TY/DynamicTexture.cpp
@@ -11,9 +11,11 @@ struct DynamicTexture::Impl
 {
     bool m_valid{};
 
-    ComPtr<ID3D12Resource> m_textureBuffer{};
     DXGI_FORMAT m_format{};
     Size m_size{};
+
+    ComPtr<ID3D12Resource> m_textureBuffer{};
+    ComPtr<ID3D12Resource> m_uploadBuffer{};
 
     Impl(const ImageView& image)
     {
@@ -41,7 +43,7 @@ struct DynamicTexture::Impl
                 &heapProperties,
                 D3D12_HEAP_FLAG_NONE,
                 &resourceDesc,
-                D3D12_RESOURCE_STATE_COPY_DEST,
+                D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
                 nullptr,
                 IID_PPV_ARGS(&m_textureBuffer));
             FAILED(hr))
@@ -50,23 +52,69 @@ struct DynamicTexture::Impl
             return;
         }
 
-        if (const HRESULT hr = m_textureBuffer->WriteToSubresource(
-                0,
-                nullptr, // リソース全体領域をコピー
-                image.getPointer(),
-                image.size.x * image.pixelSizeInBytes(),
-                image.sizeInBytes);
-            FAILED(hr))
+        const UINT64 uploadBufferSize = GetRequiredIntermediateSize(m_textureBuffer.Get(), 0, 1);
+        CD3DX12_HEAP_PROPERTIES uploadHeapProps(D3D12_HEAP_TYPE_UPLOAD);
+        CD3DX12_RESOURCE_DESC bufferDesc = CD3DX12_RESOURCE_DESC::Buffer(uploadBufferSize);
+
+        if (FAILED(EngineRenderContext::GetDevice()->CreateCommittedResource(
+            &uploadHeapProps,
+            D3D12_HEAP_FLAG_NONE,
+            &bufferDesc,
+            D3D12_RESOURCE_STATE_GENERIC_READ,
+            nullptr,
+            IID_PPV_ARGS(&m_uploadBuffer))))
         {
-            LogError(std::format("DynamicTexture: Failed to write to subresource: {}", static_cast<int>(hr)));
+            LogError("DynamicTexture: Failed to create upload buffer.");
             return;
         }
+
+        Upload(image);
 
         m_format = resourceDesc.Format;
 
         m_size = Size{static_cast<int>(resourceDesc.Width), static_cast<int>(resourceDesc.Height)};
 
         m_valid = true;
+    }
+
+    void Upload(const ImageView& image)
+    {
+        assert(m_textureBuffer && m_uploadBuffer);
+
+        if (image.size != m_size || image.format != m_format)
+        {
+            LogError("DynamicTexture: Image size or format does not match the texture buffer.");
+            return;
+        }
+
+        D3D12_SUBRESOURCE_DATA subresourceData{};
+        subresourceData.pData = image.getPointer();
+        subresourceData.RowPitch = image.size.x * image.pixelSizeInBytes();
+        subresourceData.SlicePitch = subresourceData.RowPitch * image.size.y;
+
+        // const auto commandTargetLifetime = EngineRenderContext::ScopedCommandTarget(CommandListType::Copy);
+        ID3D12GraphicsCommandList* commandList = EngineRenderContext::ActiveCommandList();
+
+        const CD3DX12_RESOURCE_BARRIER barrierBefore = CD3DX12_RESOURCE_BARRIER::Transition(
+            m_textureBuffer.Get(),
+            D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+            D3D12_RESOURCE_STATE_COPY_DEST);
+        commandList->ResourceBarrier(1, &barrierBefore);
+
+        UpdateSubresources(
+            commandList,
+            m_textureBuffer.Get(),
+            m_uploadBuffer.Get(),
+            0,
+            0,
+            1,
+            &subresourceData);
+
+        const CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(
+            m_textureBuffer.Get(),
+            D3D12_RESOURCE_STATE_COPY_DEST,
+            D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+        commandList->ResourceBarrier(1, &barrier);
     }
 };
 
@@ -79,5 +127,18 @@ namespace TY
         {
             p_impl.reset();
         }
+    }
+
+    void DynamicTexture::upload(const ImageView& image)
+    {
+        if (p_impl)
+        {
+            p_impl->Upload(image);
+        }
+    }
+
+    ID3D12Resource* DynamicTexture::getResource()
+    {
+        return p_impl ? p_impl->m_textureBuffer.Get() : nullptr;
     }
 }
--- Tsuyu/TY/DynamicTexture.h
+++ Tsuyu/TY/DynamicTexture.h
@@ -10,6 +10,10 @@ namespace TY
 
         DynamicTexture(const ImageView& image);
 
+        void upload(const ImageView& image);
+
+        ID3D12Resource* getResource();
+
     private:
         struct Impl;
         std::shared_ptr<Impl> p_impl;
