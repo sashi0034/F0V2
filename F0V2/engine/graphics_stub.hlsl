28074d47e98d71c87eb762cc81ba9e58ee0ba3f1
sashi <satoshi3104.nishi@gmail.com>
2025-08-03 15:57:54 +0900

    stub シェーダー改良

--- F0V2/engine/graphics_stub.hlsl
+++ F0V2/engine/graphics_stub.hlsl
@@ -1,16 +1,59 @@
 struct PSInput
 {
     float4 position : SV_POSITION;
+    float4 color : COLOR0;
 };
 
-PSInput VS(float4 position : POSITION)
+PSInput VS(uint id : SV_VertexID)
 {
     PSInput result;
-    result.position = position;
+
+    result.position.zw = float2(0, 1); // z = 0, w = 1
+
+    const uint n0 = 1 + id / 6;
+    const uint n1 = n0 + 1;
+
+    const uint vid = id % 6;
+
+    [flatten]
+    if (vid == 0)
+    {
+        result.position.xy = float2(0.5f / n0, 0.5f / n0); // up[0]
+    }
+    else if (vid == 1)
+    {
+        result.position.xy = float2(1.0f / n1, 0.0); // up[1]
+    }
+    else if (vid == 2)
+    {
+        result.position.xy = float2(1.0f / n0, 0.0); // up[2]
+    }
+    else if (vid == 3)
+    {
+        result.position.xy = float2(0.5f / n0, 0.5f / n0); // down[0]
+    }
+    else if (vid == 4)
+    {
+        result.position.xy = float2(0.0, 1.0f / n0); // down[1]
+    }
+    else if (vid == 5)
+    {
+        result.position.xy = float2(0.0, 1.0f / n1); // down[2]
+    }
+
+    if (n0 % 2 == 1)
+    {
+        result.color = float4(1, 1, 0, 1); // yellow
+    }
+    else
+    {
+        result.color = float4(1, 0, 1, 1); // purple
+    }
+
     return result;
 }
 
 float4 PS(PSInput input) : SV_TARGET
 {
-    return float4(1, 1, 0, 1);
+    return input.color;
 }
--- Tsuyu/TY/detail/GraphicsPipelineState.cpp
+++ Tsuyu/TY/detail/GraphicsPipelineState.cpp
@@ -105,16 +105,6 @@ struct GraphicsPipelineState::Impl : IEngineHotReloadable
 
         stub.options = GraphicsOptions{};
 
-        const bool hasPositionSemantic = stub.vertexInput.contains([&](const VertexInputElement& element)
-        {
-            return element.semanticName == "POSITION" && element.semanticIndex == 0;
-        });
-
-        if (not hasPositionSemantic)
-        {
-            stub.vertexInput = {{"POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT}};
-        }
-
         return stub;
     }
 
