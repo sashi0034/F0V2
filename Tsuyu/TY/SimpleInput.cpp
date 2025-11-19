dd60bcf1be053e2e8dd9939147b04f638056ec3e
sashi <satoshi3104.nishi@gmail.com>
2025-11-19 22:07:45 +0900

    バグ修正

--- Tsuyu/TY/SimpleInput.cpp
+++ Tsuyu/TY/SimpleInput.cpp
@@ -9,14 +9,17 @@ namespace TY
     Float2 SimpleInput::GetPlayerMovement2D()
     {
         Float2 v{};
-        v.x = (KeyD.pressed() ? 1.0f : 0.0f) - (KeyA.pressed() ? 1.0f : 0.0f);
-        v.y = (KeyS.pressed() ? 1.0f : 0.0f) - (KeyW.pressed() ? 1.0f : 0.0f);
 
-        if (v.isZero())
+        if (IsUsingGamepad())
         {
             const auto axisL = MainGamepad.axisL();
             v.x += axisL.x;
             v.y += axisL.y;
+        }
+        else
+        {
+            v.x = (KeyD.pressed() ? 1.0f : 0.0f) - (KeyA.pressed() ? 1.0f : 0.0f);
+            v.y = (KeyS.pressed() ? 1.0f : 0.0f) - (KeyW.pressed() ? 1.0f : 0.0f);
         }
 
         return v;
@@ -25,16 +28,19 @@ namespace TY
     Float3 SimpleInput::GetPlayerMovement3D()
     {
         Float3 v{};
-        v.x = (KeyD.pressed() ? 1.0f : 0.0f) - (KeyA.pressed() ? 1.0f : 0.0f);
-        v.y = (KeyE.pressed() ? 1.0f : 0.0f) - (KeyX.pressed() ? 1.0f : 0.0f);
-        v.z = (KeyW.pressed() ? 1.0f : 0.0f) - (KeyS.pressed() ? 1.0f : 0.0f);
 
-        if (v.isZero())
+        if (IsUsingGamepad())
         {
             const auto axisL = MainGamepad.axisL();;
             v.x += axisL.x;
             v.z += -axisL.y;
             v.y += (MainGamepad.rb().pressed ? 1.0f : 0.0f) - (MainGamepad.lb().pressed ? 1.0f : 0.0f);
+        }
+        else
+        {
+            v.x = (KeyD.pressed() ? 1.0f : 0.0f) - (KeyA.pressed() ? 1.0f : 0.0f);
+            v.y = (KeyE.pressed() ? 1.0f : 0.0f) - (KeyX.pressed() ? 1.0f : 0.0f);
+            v.z = (KeyW.pressed() ? 1.0f : 0.0f) - (KeyS.pressed() ? 1.0f : 0.0f);
         }
 
         return v;
@@ -43,14 +49,17 @@ namespace TY
     Float2 SimpleInput::GetCameraRotation()
     {
         Float2 v{};
-        v.x = (KeyRight.pressed() ? 1.0f : 0.0f) - (KeyLeft.pressed() ? 1.0f : 0.0f);
-        v.y = (KeyUp.pressed() ? 1.0f : 0.0f) - (KeyDown.pressed() ? 1.0f : 0.0f);
 
-        if (v.isZero())
+        if (IsUsingGamepad())
         {
             const auto axisR = MainGamepad.axisR();
             v.x += axisR.x;
             v.y += -axisR.y; // FIXME?
+        }
+        else
+        {
+            v.x = (KeyRight.pressed() ? 1.0f : 0.0f) - (KeyLeft.pressed() ? 1.0f : 0.0f);
+            v.y = (KeyUp.pressed() ? 1.0f : 0.0f) - (KeyDown.pressed() ? 1.0f : 0.0f);
         }
 
         return v;
